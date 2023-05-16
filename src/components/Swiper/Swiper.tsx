5c62e2b44a6a08cb88f2dd2cf0c9411610957592
Navid Jalilian <n.jalilian@aranumadc.local>
2023-05-16 22:06:52 +0330

    show indicator

--- src/components/Swiper/Swiper.tsx
+++ src/components/Swiper/Swiper.tsx
@@ -1,11 +1,11 @@
-import { Box, Button, Grid, Stack, Tab, Tabs, Typography } from "@mui/material";
+import { Box, Button, Stack, Typography } from "@mui/material";
 import React, { useRef, useState } from "react";
 import LapTop from "../../assets/images/laptop.jpg";
 import Arrow from "../../assets/images/arrow.svg";
 import styles from "./Swiper.style";
 import Indicator from "../Indicator/Indicator";
 
-const Swiper = ({ children, size, width }) => {
+const Swiper = ({ children, size, width, showIndicator = false }) => {
   const [Index, setIndex] = useState(0);
   const maxIndex = size - 1;
 
@@ -20,36 +20,30 @@ const Swiper = ({ children, size, width }) => {
       setIndex((prevIndex) => prevIndex + 1);
     }
   };
-  return (
-    <Grid container>
-      <Grid item xs={10} sx={styles.swiperContainer}>
-        <Box sx={styles.swiperContent}>
-          <Box
-            style={{
-              flexGrow: 1,
-              transition: "transform 0.3s ease-in-out",
 
-              width,
-              transform: `translateX(-${Index * 100}%)`,
-            }}
-          >
-            {children}
-          </Box>
-        </Box>
-      </Grid>
-      <Grid item xs={10}>
-        <Stack
-          direction="row"
-          alignItems="center"
-          justifyContent="space-around"
+  return (
+    <Box>
+      <Box sx={{ ...styles.swiperContainer, width }}>
+        <Box
+          sx={styles.swiperContent}
+          style={{
+            flexGrow: 1,
+            transition: "transform 0.3s ease-in-out",
+            width,
+            transform: `translateX(-${Index * 100}%)`,
+          }}
         >
-          <Indicator {...{ Index, setIndex }} />
-          <Button onClick={handleNextClick} disabled={Index === maxIndex}>
-            <Box sx={{}} component="img" src={Arrow} />
-          </Button>
-        </Stack>  
-      </Grid>
-    </Grid>
+          {children}
+        </Box>
+      </Box>
+      <Stack direction="row" alignItems="center" justifyContent="space-around">
+        {showIndicator && <Indicator {...{ Index, setIndex }} />}
+        <Button onClick={handleNextClick} disabled={Index === maxIndex}>
+          <Box component="img" src={Arrow} />
+        </Button>
+      </Stack>
+    </Box>
   );
 };
+
 export default Swiper;
