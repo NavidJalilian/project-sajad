06c890ba637fa618324f8df6ffc9f9b4ec9ad882
Mohammad Ali <forrest482@outlook.com>
2023-05-16 19:55:38 +0330

    services

--- src/components/Services/Services.style.ts
+++ src/components/Services/Services.style.ts
@@ -3,56 +3,97 @@ import { StylesType } from "../../theme/type";
 const styles: StylesType<"root" | "slides" | "content" | "imgLabel"> = {
   root: {
     label: "services",
+
     backgroundColor: ({ palette }) => palette.grey["100"],
+
     mt: "50px",
-    display: "flex",
   },
+
   container: {
     label: "servicesContainer",
+
     display: "flex",
+
     flexDirection: "column",
+
     justifyContent: "center",
+
     alignItems: "center",
+
     width: "100%",
   },
+
   slogan: {
     label: "slogan",
+
     fontFamily: "Outfit",
+
     fontStyle: "normal",
+
     fontWeight: 700,
+
     fontSize: "70px",
+
     lineHeight: "120.5%",
+
     letterSpacing: "0.033em",
+
     pt: "37px",
+
     color: ({ palette }) => palette.primary.dark,
+
     ".textRed": {
       color: ({ palette }) => palette.error.main,
     },
   },
+
   slides: {
     label: "images",
+
     display: "flex",
+
     overflow: "hidden",
+
     width: 500,
+
     flexDirection: "column",
+
     gap: 1,
+
     flexGrow: 1,
+
     flexShrink: 0,
   },
+
   imgLabel: {
     fontFamily: "'Outfit', sans-serif",
+
     fontStyle: "normal",
+
     fontWeight: 300,
+
     fontSize: "50px",
+
     lineHeight: "120.5%",
+
     letterSpacing: "0.033em",
-  
+
     display: "flex",
+
     mt: "112px",
   },
+
+  content: {
+    display: "flex",
+
+    mt: "112px",
+  },
+
   bannerArrowBtn: {
     position: "absolute",
+
     bottom: "20px",
+
     right: 0,
   },
 };
--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -1,25 +1,41 @@
 import React from "react";
+
 import { Box, Button, Stack, Typography } from "@mui/material";
+
 import styles from "./Services.style";
+
 import InfoRightImg from "../../assets/images/info-right.jpg";
+
 import InfoMiddleImg from "../../assets/images/info-middle.jpg";
+
 import InfoLeftImg from "../../assets/images/info-left.jpg";
+
 import RightArrowIcon from "../../assets/images/rightArrow.svg";
+
 import RightArrowRedIcon from "../../assets/images/rightArrowRed.svg";
+
 import BannerImg from "../../assets/images/banner.png";
+
 import Swiper from "../Swiper";
+
 import Indicator from "../Indicator/Indicator";
+
 import Banner from "../Banner";
 
 const images = [
   [
     { url: InfoLeftImg, label: "Commercials" },
+
     { url: InfoMiddleImg, label: "Consulting" },
+
     { url: InfoRightImg, label: "Company" },
   ],
+
   [
     { url: InfoLeftImg, label: "Personnel" },
+
     { url: InfoMiddleImg, label: "Services" },
+
     { url: InfoRightImg, label: "Organization" },
   ],
 ];
@@ -34,6 +50,7 @@ export default function Services() {
 
         <Box mt={2} position="relative" width="100%">
           <Box src={BannerImg} height={300} component="img" />
+
           <Button sx={styles.bannerArrowBtn} variant="text">
             <img src={RightArrowRedIcon} width={39} height={36} />
           </Button>
@@ -42,81 +59,99 @@ export default function Services() {
         <Banner />
 
         <Box sx={styles.content} className="container">
-          <Swiper size={images.length} width={500} height={860}>
-            <Stack direction="row" width={500}>
-              {images.map((row) => (
-                <Box sx={styles.slides}>
-                  {row.map((image, idx) => (
-                    <Box
-                      sx={{
-                        position: "relative",
-                      }}
-                    >
-                      <Box
-                        component="img"
-                        sx={{
-                          flexGrow: 1,
-                          flexShrink: 0,
-                        }}
-                        src={image.url}
-                        alt={image.label}
-                        key={image.url}
-                        width={500}
-                        height={286}
-                      />
-                      {/* {idx > 0 && ( */}
+          <Box>
+            <Swiper size={images.length} width={500} height={860}>
+              <Stack direction="row" width={500}>
+                {images.map((row) => (
+                  <Box sx={styles.slides}>
+                    {row.map((image, idx) => (
                       <Box
                         sx={{
-                          position: "absolute",
-                          bottom: 20,
-                          left: 0,
-                          color: ({ palette }) => palette.primary.light,
-                          display: "flex",
-                          gap: 2,
-                          mx: 2,
-                          width: "100%",
+                          position: "relative",
                         }}
                       >
-                        <Typography sx={styles.imgLabel}>
-                          {String(idx).length < 2 ? "0" + idx : idx}
-                        </Typography>
-                        <Typography sx={styles.imgLabel}>
-                          {image.label}
-                        </Typography>
+                        <Box
+                          component="img"
+                          sx={{
+                            flexGrow: 1,
+
+                            flexShrink: 0,
+                          }}
+                          src={image.url}
+                          alt={image.label}
+                          key={image.url}
+                          width={500}
+                          height={286}
+                        />
+
+                        {/* {idx > 0 && ( */}
+
+                        <Box
+                          sx={{
+                            position: "absolute",
+
+                            bottom: 20,
+
+                            left: 0,
+
+                            color: ({ palette }) => palette.primary.light,
+
+                            display: "flex",
+
+                            justifyContent: "space-between",
+
+                            width: "100%",
+                          }}
+                        >
+                          <Typography sx={styles.imgLabel}>
+                            {String(idx).length < 2 ? "0" + idx : idx}
+                          </Typography>
+
+                          <Typography sx={styles.imgLabel}>
+                            {image.label}
+                          </Typography>
+                        </Box>
+
+                        {/* )} */}
                       </Box>
-                      {/* )} */}
-                    </Box>
-                  ))}
+                    ))}
+                  </Box>
+                ))}
+              </Stack>
+            </Swiper>
+
+            <Indicator value={2} />
+          </Box>
+
+          <Box>
+            <Typography variant="h1" sx={styles.mainTitle}>
+              Our
+            </Typography>
+
+            <Typography variant="h1" sx={styles.mainTitle} className="redText">
+              Services
+            </Typography>
+
+            <Typography variant="body2" sx={styles.text}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </Typography>
+
+            <Button sx={styles.button} variant="contained">
+              <Stack
+                direction="row"
+                justifyContent="space-around"
+                width="100%"
+                alignItems="center"
+                py={0.5}
+              >
+                <Typography>Explore More</Typography>
+
+                <Box>
+                  <img src={RightArrowIcon} width={22} height={20} />
                 </Box>
-              ))}
-            </Stack>
-          </Swiper>
-          <Indicator value={2} />
-        </Box>
-        <Box sx={styles.content} className="container">
-          <Typography variant="h1" sx={styles.mainTitle}>
-            Our
-          </Typography>
-          <Typography variant="h1" sx={styles.mainTitle} className="redText">
-            Services
-          </Typography>
-          <Typography variant="body2" sx={styles.text}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          </Typography>
-          <Button sx={styles.button} variant="contained">
-            <Stack
-              direction="row"
-              justifyContent="space-around"
-              width="100%"
-              alignItems="center"
-              py={0.5}
-            >
-              <Typography>Explore More</Typography>
-              <Box>
-                <img src={RightArrowIcon} width={22} height={20} />
-              </Box>
-            </Stack>
-          </Button>
+              </Stack>
+            </Button>
+          </Box>
         </Box>
       </Box>
     </Box>
